3fdc8b59e73d11e91d485a8ab9f48e5c7d8889e8
Christian <christiangoodridge@gmail.com>
2016-07-29 00:29:11 -0230

    Falloff Toggle V6.1

--- pokeconfig.py
+++ pokeconfig.py
@@ -21,7 +21,7 @@ class Pokeconfig:
     num_steps = DEFAULT_NUM_STEPS
     distance_unit = DEFAULT_DISTANCE_UNIT
     slack_name = str(os.environ['SLACK_NAME'])
-    falloff = bol(os.environ['FALLOFF'])
+    falloff = bool(os.environ['FALLOFF'])
     position = ()
 
     def load_config(self, config_path):
